5eacda746f65cdec9761b4adb649f696c174eea7
laxa <laxa@sbim.be>
2020-05-05 12:56:09 +0200

    Project 3 complete

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1-enfant.component.css']
 })
 export class Exercice1EnfantComponent implements OnInit {
-
+  @Input() compteur: number;
+  @Output() updateCounter: EventEmitter<number> = new EventEmitter<number>();
+  public increaseValue = 1 ;
+  public decreaseValue = 1 ;
   constructor() { }
 
   ngOnInit() {
   }
 
+
+  increaseCounter(increaseValue: number) {
+    this.compteur += increaseValue;
+    this.updateCounter.emit(this.compteur);
+  }
+
+  decreaseCounter(decreaseValue: number) {
+    this.compteur -= decreaseValue;
+    this.updateCounter.emit(this.compteur);
+  }
 }
